05ae87134b59485774637289ea2ae26f28727a39
William <willtheorangeguy@users.noreply.github.com>
2018-10-06 10:55:39 -0700

    Add white space to the bottom of the file

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-#Prompt
+# Copyright (C) 2018 William Vandergraaf
+# All rights reserved. To learn more type 'copyright' at the prompt.
+
+# Import Statements
+import os
+
+# Prompt
 INPUTmain = input('LEGO CMD: ')
 
-#Functions to answer commands
+# Functions to answer commands
+    # Piece commands
 if INPUTmain == 'newpiece': #the 'newpiece' command
     global INPUTnewpiece #create global var
     global INPUTnewpiececount #create global var
     global INPUTnewpiececolour #create global var
     INPUTnewpiece = str(input('Name the piece you would like to add: ')) #ask for name
-    INPUTnewpiececolour = str(input('What is the piece colour (make sure this colour is in the colour database, otherwise add new using \'newcolour\')? ')) #ask for colour
+    INPUTnewpiececolour = str(input('What is the piece colour (make sure this colour is in the colour database, otherwise add using "newcolour")? ')) #ask for colour
     INPUTnewpiececount = int(input('How many of that would you like to add? ')) #ask for qty
     print('A new piece has been created named:', '"' + INPUTnewpiece + '",', 'in the colour:', '"' + INPUTnewpiececolour + '",', 'in the quantity of:', INPUTnewpiececount) #check with user to make sure input ok
-    
+
 elif INPUTmain == 'newcolour': #the 'newcolour' command
     global INPUTnewcolour #create global var
     INPUTnewcolour = str(input('Name the colour you would like to add: ')) #ask for colour name
@@ -34,31 +41,84 @@ elif INPUTmain == 'removepiece': #the 'removepiece' command
     INPUTpiececount = int(input('How many of that piece do you want to remove? ')) #ask for piece qty
     print(INPUTpiececount, 'pieces have been removed from:', '"' + INPUTpiecename + '",', 'in the colour of:', '"' + INPUTpiececolour + '".') #check with user to make sure input ok
 
-elif INPUTmain == 'sort-all': #the 'sort-all' command
-    PIECESall = {'piecename':'piecenames', 'piececolour':'piecescolour', 'piececount':'noofpieces'} #create dictionary of pieces requested
+elif INPUTmain == 'sortparts-all': #the 'sortparts-all' command
+    PIECESall = {'piecename':'piecenames', 'piececolour':'piecescolour', 'piececount':'numofpieces'} #create dictionary of pieces requested
     print('Here are all the pieces in the database:', PIECESall) #return specified results
 
-elif INPUTmain == 'sort-name': #the 'sort-name' command
+elif INPUTmain == 'sortparts-name': #the 'sortparts-name' command
     global INPUTsearch #create global var
     INPUTsearch = str(input('Please enter a brick name: ')) #search bar to sort by name
-    PIECESbyname = {'piecename':'piecenamescontainingINPUTsearch', 'piececolour':'piecescolour', 'piececount':'noofpieces'} #create dictionary of pieces requested
+    PIECESbyname = {'piecename':'piecenamescontainingINPUTsearch', 'piececolour':'piecescolour', 'piececount':'numofpieces'} #create dictionary of pieces requested
     print('The following results were found for this search keyword:', '"' + INPUTsearch + '", \n', PIECESbyname) #return specified results
 
-elif INPUTmain == 'sort-colour': #the 'sort-colour' command
+elif INPUTmain == 'sortparts-colour': #the 'sortparts-colour' command
     INPUTsearch = str(input('Please enter a brick colour: ')) #search bar to sort by colour
-    PIECESbycolour = {'piecename':'piecenames', 'piececolour':'piecescolourcontainingINPUTsearch', 'piececount':'noofpieces'} #create dictionary of pieces requested
+    PIECESbycolour = {'piecename':'piecenames', 'piececolour':'piecescolourcontainingINPUTsearch', 'piececount':'numofpieces'} #create dictionary of pieces requested
     print('The following results were found for this colour:', '"' + INPUTsearch + '", \n', PIECESbycolour) #return specified results
-elif INPUTmain == 'sort-color': #the 'sort-color' command
+elif INPUTmain == 'sortparts-color': #the 'sortparts-color' command
     INPUTsearch = str(input('Please enter a brick color: ')) #search bar to sort by colour
-    PIECESbycolour = {'piecename':'piecenames', 'piececolour':'piecescolourcontainingINPUTsearch', 'piececount':'noofpieces'} #create dictionary of pieces requested
+    PIECESbycolour = {'piecename':'piecenames', 'piececolour':'piecescolourcontainingINPUTsearch', 'piececount':'numofpieces'} #create dictionary of pieces requested
     print('The following results were found for this color:', '"' + INPUTsearch + '", \n', PIECESbycolour) #return specified results
 
+    # Set commands
+elif INPUTmain == 'newset': #the 'newset' command
+    global INPUTnewset #create global var
+    global INPUTnewsetnum #create global var
+    global INPUTnewsettheme #create global var
+    global INPUTnewsetpiececount #create gloabl var
+    global INPUTnewsetcount #create global var
+    INPUTnewset = str(input('Name the set you would like to add: ')) #ask set name
+    INPUTnewsetnum = str(input('What is the set number for the set you would like to add? ')) #ask set number
+    INPUTnewsettheme = str(input('What is the theme for the set you would like to add? (make sure this theme is in the database, otherwise add using "newtheme") ')) #ask set theme
+    INPUTnewsetpiececount = str(input('How many pieces are there in this set? ')) #ask set piece count
+    INPUTnewsetcount = int(input('How many of this set would you like to add? ')) #ask number of sets to add
+    print('A new set has been created named:', '"' + INPUTnewset + '",', 'with the set number:', '"' + INPUTnewsetnum + '",', 'in the theme:', '"' + INPUTnewsettheme + '",', 'with the piece count of:', INPUTnewsetpiececount + ',', 'in the quantity of:', INPUTnewsetcount) #check with user to make sure input ok
+
+elif INPUTmain =='newtheme': #the 'newtheme' command
+    global INPUTnewtheme #create global var
+    INPUTnewtheme = str(input('Name the theme you would like to add: ')) #ask for theme name
+    print('A new theme had been created named:', '"' + INPUTnewtheme + '"') #check with user to make sure input ok
+
+elif INPUTmain == 'addset': #the 'addset' command
+    global INPUTsetnum #create global var
+    global INPUTsetcount #create global var
+    INPUTsetnum = str(input('What is the set number of the set that you would like to add? ')) #ask for set number
+    INPUTsetcount = int(input('How many of that set do you want to add? ')) #ask for set qty
+    print(INPUTsetcount, 'sets have been added to set number:', '"' + INPUTsetnum + '"') #check with user to make sure input ok
+
+elif INPUTmain == 'removeset': #the 'removeset' command
+    INPUTsetnum = str(input('What is the set number of the set that you would like to remove? ')) #ask for set number
+    INPUTsetcount = int(input('How many of that set do you want to remove? ')) #ask for set qty
+    print(INPUTsetcount, 'sets have been removed from set number:', '"' + INPUTsetnum + '"') #check with user to make sure input ok
+
+elif INPUTmain == 'sortsets-all': #the 'sortsets-all' command
+    SETSall = {'setname':'setnames', 'setnum':'setnums', 'settheme':'setthemes', 'setpiececount':'numofpieces', 'setcount':'numofsets'} #create dictionary of sets requested
+    print('Here are all the sets in the database:', SETSall) #return specified results
+
+elif INPUTmain == 'sortsets-name': #the 'sortsets-name' command
+    INPUTsearch = str(input('Please enter a set name (NOT number): ')) #search bar to sort by name
+    SETSbyname = {'setname':'setnamescontainingINPUTsearch', 'setnum':'setnums', 'settheme':'setthemes', 'setpiececount':'numofpieces', 'setcount':'numofsets'} #create dictionary of pieces requested
+    print('The following results were found for this search keyword:', '"' + INPUTsearch + '", \n', SETSbyname) #return specified results
+
+elif INPUTmain == 'sortsets-number': #the 'sortsets-number' command
+    INPUTsearch = str(input('Please enter a set number (NOT name): ')) #search bar to sort by name
+    SETSbynum = {'setname':'setnames', 'setnum':'setnumscontainingINPUTsearch', 'settheme':'setthemes', 'setpiececount':'numofpieces', 'setcount':'numofsets'} #create dictionary of pieces requested
+    print('The following results were found for this search keyword:', '"' + INPUTsearch + '", \n', SETSbynum) #return specified results
+
+elif INPUTmain == 'sortsets-theme': #the 'sortsets-theme' command
+    INPUTsearch = str(input('Please enter a set theme: ')) #search bar to sort by name
+    SETSbytheme = {'setname':'setnames', 'setnum':'setnums', 'settheme':'setthemescontainingINPUTsearch', 'setpiececount':'numofpieces', 'setcount':'numofsets'} #create dictionary of pieces requested
+    print('The following results were found for this search keyword:', '"' + INPUTsearch + '", \n', SETSbytheme) #return specified results
+
+
+    # Other commands
 elif INPUTmain == 'help': #the 'help' command
     print('You have entered the help area! Here is a list of commands for your reference:') #help title
-    print('"newpiece" - creates a new piece in the database. \n"newcolour/newcolor" - creates a new colour in the database. \n"addpiece" - adds more of your selected piece to the database. \n"removepiece" - removes a quantity of your selected piece from the database. \n"sort-all" - prints all pieces and their respective colours and quantities to the screen. \n"sort-name" - prints all pieces related to the search query entered, as well as their respective colours and quantities to the screen. \n"sort-colour/sort-color" - prints all pieces in that colour, as well as their respective names and quantities. \n"help" - prints a list of commands, and is where you are right now.') #list of functions
+    print('"newpiece" - creates a new piece in the database. \n"newcolour/newcolor" - creates a new colour in the database. \n"addpiece" - adds a quantity of your selected piece to the database. \n"removepiece" - removes a quantity of your selected piece from the database. \n"sortparts-all" - prints all pieces and their respective colours and quantities to the screen. \n"sortparts-name" - prints all pieces related to the search query entered, as well as their respective colours and quantities to the screen. \n"sortparts-colour/sortparts-color" - prints all pieces in that colour, as well as their respective names and quantities. \n"newset" - creates a new set in the database. \n"newtheme" - creates a new theme in the database. \n"addset" - adds a quantity of your selected set to the database. \n"removeset" - removes a quantity of your selected set from the database. \n"sortsets-all" - prints all sets and their respective set numbers, themes, piece count and quantity to the screen. \n"sortsets-name" - prints all the sets related to the set name entered, as well as their respective set numbers, themes, piece count and quantity to the screen. \n"sortsets-number" - prints all the sets related to the set number entered, as well as their respective set names, themes, piece count and quantity to the screen. \n"sortsets-theme" - prints all the sets related to the set theme entered, as well as their respective set names, numbers, piece count and quantity to the screen. \n"copyright/license" - prints license and copyright information and opens the license in default text editor. \n"help" - prints a list of commands, and is where you are right now.') #list of functions
 
-elif INPUTmain == 'copyright' or 'license':
-    print('Copyright (C) 2018 Dog Face Development Co. \nUse is subject to the terms and conditions outlined in the LICENSE.md document.')
+elif INPUTmain == 'copyright' or 'license': #the 'copyright command'
+    print('Copyright (C) 2018 Dog Face Development Co. \nUse is subject to the terms and conditions outlined in the LICENSE.md document.') #copyright info
+    os.startfile("C:\\Users\\William Vandergraaf\\Documents\\Dog Face Development Co\\Legal\\Project Licences\\GNU General Public License.txt") #opens license
 
 else:
     print('Sorry, try again! \nType "help" for a list of commands.') #helpful error message
