b5a8bd4eb5966d42ab24e4e4ac299ab92be5f1dd
William <willtheorangeguy@users.noreply.github.com>
2023-12-04 18:19:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-# pylint: disable=invalid-name, global-variable-undefined, too-many-branches, too-many-statements, consider-using-in
+#pylint: disable=invalid-name, global-variable-undefined, too-many-branches, too-many-statements, consider-using-in
 
 
 def lego_cmd():
