16f285b115d6edf4650907ae843bc7cb4de104ca
Restyled.io <commits@restyled.io>
2023-12-07 00:25:16 +0000

    Restyled by isort

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,11 @@
 """Tests for main.py."""
 # pylint: disable=import-error, wrong-import-position, unused-argument, line-too-long
 
-import main
-import sys
 import os
-from unittest.mock import patch, call
+import sys
+from unittest.mock import call, patch
+
+import main
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
